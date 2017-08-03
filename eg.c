825dd79dfea2ee8ab45eab95eafbd012f92845a0
Dave Pearson <davep@davep.org>
2017-08-03 14:59:31 +0100

    Tweak how the licence is shown when -v is used
    It wasn't showing well at all on my iMac.

--- eg.c
+++ eg.c
@@ -174,24 +174,22 @@ static void ShowVersionInfo( void )
     uname( &utsn );
 
     printf( "Expert Guide Version " EG_VERSION "\n\n"
-            "\
-     Expert Guide - A Text Mode Norton Guide Reader\
-     Copyright (C) 1997-2015 David A Pearson\
-\
-     This program is free software; you can redistribute it and/or modify\
-     it under the terms of the GNU General Public License as published by\
-     the Free Software Foundation; either version 2 of the license, or\
-     (at your option) any later version.\
-\
-     This program is distributed in the hope that it will be useful,\
-     but WITHOUT ANY WARRANTY; without even the implied warranty of\
-     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\
-     GNU General Public License for more details.\
-\
-     You should have received a copy of the GNU General Public License\
-     along with this program; if not, write to the Free Software\
-     Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.\
-\n" );
+            "Expert Guide - A Text Mode Norton Guide Reader\n"
+            "Copyright (C) 1997-2015 David A Pearson\n"
+            "\n"
+            "This program is free software; you can redistribute it and/or modify\n"
+            "it under the terms of the GNU General Public License as published by\n"
+            "the Free Software Foundation; either version 2 of the license, or\n"
+            "(at your option) any later version.\n"
+            "\n"
+            "This program is distributed in the hope that it will be useful,\n"
+            "but WITHOUT ANY WARRANTY; without even the implied warranty of\n"
+            "MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\n"
+            "GNU General Public License for more details.\n"
+            "\n"
+            "You should have received a copy of the GNU General Public License\n"
+            "along with this program; if not, write to the Free Software\n"
+            "Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.\n\n" );
 
     printf( "System details:\n"
             "\tSystem.......: %s %s\n"
